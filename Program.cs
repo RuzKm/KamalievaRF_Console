0e856216882e54153541bddb5559b9383a0d77d7
KamalievaRF <rruzzi@yandex.ru>
2025-05-28 17:31:04 +0300

    Add *.cs-files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace KamalievaRF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //KamalievaRF Am-1-24
+            //solve task3
+            
+        }
+        public static double Sqr(double X)
+        {
+            return X * X;
+        }
+
+    }
+}
